10c41fbf356455bec77c417b8540dda07c0065ce
nadahasnim <nadasthing@gmail.com>
2019-11-25 16:00:04 +0700

    membuat landing page
    halaman awal dari aplikasi

---
+++ index.php
@@ -0,0 +1,330 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- custom css -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Raleway&display=swap" rel="stylesheet">
+
+  <!-- fontawesome icon -->
+  <script src="https://kit.fontawesome.com/9afba118d6.js" crossorigin="anonymous"></script>
+
+  <title>INDIEKOS | Landing Page</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">INDIEKOST</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link" href="#">Tentang <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Keunggulan</a>
+            <a class="nav-item nav-link" href="#">Testimoni</a>
+            <a class="nav-item btn btn-light tombol tombol-nav" href="#" tabindex="-1" aria-disabled="true">SIGN
+              UP</a>
+            <a class="nav-item btn btn-light tombol tombol-nav" href="#" tabindex="-1" aria-disabled="true">SIGN
+              IN</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- navbar end -->
+
+  <main>
+    <!-- jumbotron -->
+    <div class="jumbotron jumbotron-atas jumbotron-fluid">
+      <div class="container banner-tittle">
+        <h3>WELCOME</h3>
+        <h1 class="display-4">RUMAH KOST BIDADARI</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nesciunt beatae
+          obcaecati ex
+          repudiandae consequatur magni dicta, aut ea mollitia amet reprehenderit dolorem repellat eligendi qui
+          numquam a
+          cumque esse.</p>
+        <a href="" class="btn up-1 btn-gradient">Cek Ketersediaan Kamar</a>
+      </div>
+    </div>
+    <!-- jumbotron -->
+
+    <!-- tentang -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <h2 class="tentang pt-5">Rumah Kost Ini Menggunakan Layanan <span class="biru">INDIEKOS</span> <br>
+            Software Pengelolaan Rumah Kost. <span class="biru">Simple, Fleksibel, Mudah Digunakan</span>
+          </h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <p class="text-muted text-center mt-2 ">Mengapa pemilik kost perlu INDIEKOST?</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col-12 col-md-6 pb-3">
+          <!-- img preview1 -->
+          <img src="img/preview1.jpg" class="img-fluid mt-n3" alt="Indiekost Mockup">
+        </div>
+        <div class="col col-12 col-md-6 pt-md-3 mt-md-5 pr-5">
+          <!-- deskripsi -->
+          <h4 class="font-weight-bold">Diperlukan Untuk Mengelola Rumah Kos Secara Mandiri</h4>
+          <p class="text-muted">Integrasi semua operasi penting dalam satu platform</p>
+          <p class="font-weight-bold text-break">
+            <i class="fas fa-check-circle text-success"></i> Pengelolaan Kamar dan Penghuni/Penyewa <br>
+            <i class="fas fa-check-circle text-success"></i> Check in & Check out <br>
+            <i class="fas fa-check-circle text-success"></i> Laporan keuangan & Laba-rugi <br>
+            <i class="fas fa-check-circle text-success"></i> Inventarisasi
+
+          </p>
+          <p class="text-muted">Anda bisa mendapatkan informasi terkini secara <span
+              class="font-weight-bold font-italic text-muted text-break">real time</span>, kapan saja, dimana saja</p>
+        </div>
+      </div>
+    </div>
+    <!-- tentang -->
+
+    <!-- keunggulan atas-->
+    <div class="container-fluid bg-light pt-5 pb-4">
+      <div class="container">
+        <div class="row">
+          <!-- kiri -->
+          <div class="col col-12 col-md-6">
+            <!-- kiri atas -->
+            <div class="row">
+              <div class="col col-2">
+                <i class="fas fa-shield-alt ikon-besar"></i>
+              </div>
+              <div class="col col-10">
+                <h6 class="font-weight-bold">Mencegah Kecurangan Biaya & Pendapatan</h6>
+                <p class="text-muted">
+                  Catatan check in & check out, pemasukan, dan pengeluaran tercatat rapi sehingga akan meminimalisir
+                  kecurangan staff
+                </p>
+              </div>
+            </div>
+
+            <!-- kiri bawah -->
+            <div class="row mt-md-4">
+              <div class="col col-2">
+                <i class="fas fa-clock ikon-besar"></i>
+              </div>
+              <div class="col col-10">
+                <h6 class="font-weight-bold">Full Online 24-Jam</h6>
+                <p class="text-muted">
+                  Dimana pun lokasi kost Anda, manajemen dan perkembangan bisnis bisa diakses secara realtime
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <!-- kanan -->
+          <div class="col col-12 col-md-6">
+            <!-- kanan atas -->
+            <div class="row">
+              <div class="col col-2">
+                <i class="fas fa-chart-line ikon-besar"></i>
+              </div>
+              <div class="col col-10">
+                <h6 class="font-weight-bold">Meningkatkan Hunian Kost</h6>
+                <p class="text-muted">
+                  INDIEKOST membantu meningkatkan hunian kost. Kami juga memampangkan informasi tentang rumah kost Anda
+                  di halaman Homepage, sebagai bussiness profile dari rumah kost Anda
+                </p>
+              </div>
+            </div>
+
+            <!-- kanan bawah -->
+            <div class="row">
+              <div class="col col-2">
+                <i class="fas fa-shopping-cart ikon-besar"></i>
+              </div>
+              <div class="col col-10">
+                <h6 class="font-weight-bold">Reservasi Online</h6>
+                <p class="text-muted">
+                  Reservasi bisa dilakukan secara Online, kapan saja dan dimana saja
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- keunggulan atas-->
+
+    <div class="container bg-keunggulan">
+      <!-- keuunggulan deskripsi -->
+      <div class="row mt-5">
+        <div class="container">
+          <h5 class="font-weight-bold text-center">Keunggulan INDIEKOST</h5>
+          <p class="text-muted text-center">INDIEKOST merupakan aplikasi pengelola rumah kos berbasis online (WEB) yang
+            akan membantu meningkatkan produktivitas usaha Anda. Satu aplikasi, banyak solusi. <span
+              class="text-ungu font-weight-bold">Didesain untuk orang awam IT</span></p>
+        </div>
+      </div>
+
+      <!-- keunggulan dengan gambar -->
+      <div class="row mt-3 pb-5">
+        <!-- kiri -->
+        <div class="col col-12 col-md-6">
+
+          <div class="row">
+            <div class="col col-2">
+              <div class="lingkar">1</div>
+            </div>
+            <div class="col col-10">
+              <h6 class="font-weight-bold ml-2">Sistem yang handal dan lengkap</h6>
+              <p class="text-muted ml-2">Tersedia semua hal yang Anda butuhkan untuk mengelola rumah kos Anda</p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col col-2">
+              <div class="lingkar mt-2">2</div>
+            </div>
+            <div class="col col-10">
+              <h6 class="font-weight-bold ml-2">Multi device, multi platform</h6>
+              <p class="text-muted ml-2">Yang Anda butuhkan hanyalah koneksi internet dan web browser. Aplikasi dapat
+                diakses dari mana saja dan kapan saja</p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col col-2">
+              <div class="lingkar mt-2">3</div>
+            </div>
+            <div class="col col-10">
+              <h6 class="font-weight-bold ml-2">Mudah digunakan</h6>
+              <p class="text-muted ml-2">Tampilan tatap muka didesain sedemikian rupa sehingga mudah digunakan oleh
+                pengguna. Dan memenuhi standar UI/UX yang ada</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- kanan -->
+        <div class="col col-12 col-md-6">
+          <img src="img/preview2.png" class="img-fluid mt-n3 scale" alt="Indiekost Mockup">
+        </div>
+      </div>
+    </div>
+
+
+    <!-- testimoni -->
+    <!-- jumbotron -->
+    <div class="jumbotron testimoni jumbotron-fluid">
+      <div class="container item">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="text-light text-center font-weight-bold">Testimoni</h2>
+            <p class="text-center text-light">Apa kata mereka yang telah menggunakan INDIEKOST</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="testmonial_slider_area text-center owl-carousel">
+                  <div class="row">
+                    <div class="col col-12 col-md-6">
+                      <div class="single_testimonial">
+                        <div class="pic">
+                          <img src="img/agus.jpg" alt="">
+                        </div>
+                        <h5 class="testimonial-title">Agus Sugiono</h5>
+                        <span class="test_designation">Pemilik Kost</span>
+                        <p class="text-muted">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto blanditiis, aliquid sit itaque
+                          fuga ad exercitationem impedit atque? Excepturi nobis nostrum architecto, veniam molestiae
+                          dolor magnam facilis aliquam! Quod, quam.
+                        </p>
+                      </div><!-- end Single Testimonials -->
+                    </div>
+
+                    <div class="col col-12 col-md-6">
+                      <div class="single_testimonial">
+                        <div class="pic">
+                          <img src="img/siti.jpg" alt="">
+                        </div>
+                        <h5 class="testimonial-title">Siti Memunah</h5>
+                        <span class="test_designation">Penghuni Kost</span>
+                        <p class="text-muted">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque non enim dolor corporis in
+                          dolore quidem nobis officia culpa laudantium iste qui dolores, obcaecati iusto nesciunt quae,
+                          beatae nam nihil.
+                        </p>
+                      </div><!-- end Single Testimonials -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- jumbotron -->
+    <!-- testimoni -->
+
+
+    <!-- bergabung -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <h2 class="text-center font-weight-bolder mt-3 mb-3">BERGABUNG DENGAN INDIEKOST SEKARANG</h2>
+        </div>
+      </div>
+      <div class="row">
+        <a href="" class="btn btn-gradient-big mb-5">BERGABUNG</a>
+      </div>
+    </div>
+    <!-- bergabung -->
+  </main>
+
+  <!-- footer -->
+  <footer>
+  <!-- footer atas -->
+  <div class="container-fluid bg-dark text-light">
+    <div class="row">
+      <div class="col col-12 col-md-3">1</div>
+      <div class="col col-12 col-md-3">2</div>
+      <div class="col col-12 col-md-3">3</div>
+      <div class="col col-12 col-md-3">4</div>
+    </div>
+    <div class="row">
+      <p class="text-muted">Powered by TeknoTirta</p>
+    </div>
+  </div> <!-- akhir footer atas -->
+  </footer>
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
